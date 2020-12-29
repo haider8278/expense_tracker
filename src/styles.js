4f2de70ab74fd1a34ac5c44b0693ac4d4c9dec24
asad585 <asad585@hotmail.com>
2020-12-29 16:29:11 +0500

    style

--- src/styles.js
+++ src/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    padding:50,
+    padding:20,
   },
   paper: {
     padding: theme.spacing(2),
